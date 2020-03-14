581e44c3dbc2131ed1529fe8ab934f229affe069
mrw1593 <botahamec@outlook.com>
2020-03-14 17:20:07 -0400

    Ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 extern crate serenity;
 
-use serenity::prelude::*;
-use serenity::model::gateway::Ready;
 use serenity::model::channel::Message;
 use serenity::model::channel::Reaction;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
 
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 
 struct Handler;
 
 impl EventHandler for Handler {
-
     fn reaction_add(&self, ctx: Context, reaction: Reaction) {
-        if let Err(why) = reaction.channel_id.say(&ctx.http, format!("{} left a reaction", reaction.user_id)) {
+        if let Err(why) = reaction
+            .channel_id
+            .say(&ctx.http, format!("{} left a reaction", reaction.user_id))
+        {
             println!("Error reacting to a reaction: {:?}", why);
         }
     }
@@ -32,13 +34,12 @@ impl EventHandler for Handler {
 }
 
 fn main() {
-
     let mut file = File::open("token").unwrap();
     let mut token = String::new();
-    file.read_to_string(&mut token).expect("Token file not found");
+    file.read_to_string(&mut token)
+        .expect("Token file not found");
 
-    let mut client = Client::new(&token, Handler)
-                        .expect("Error creating client");
+    let mut client = Client::new(&token, Handler).expect("Error creating client");
 
     if let Err(msg) = client.start() {
         println!("Error: {:?}", msg);
