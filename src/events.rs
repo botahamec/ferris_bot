a19082f3e6112ad52fc40050c5c61c32644dc2f5
mrw1593 <botahamec@outlook.com>
2020-04-04 12:02:57 -0400

    Set up commands

---
+++ src/events.rs
@@ -0,0 +1,45 @@
+use serenity::{
+	prelude::{EventHandler, Context},
+	model::{channel::{Message, Reaction, ReactionType}, gateway::Ready}
+};
+
+pub struct Handler;
+
+impl EventHandler for Handler {
+
+	/* says pong on "!ping"
+	fn message(&self, ctx: Context, msg: Message) {
+		if msg.content == "?ping" {
+			if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?") {
+				println!("Error giving message: {:?}", why);
+			}
+		}
+	}*/
+
+	// says a message whenever a reaction is made
+	fn reaction_add(&self, ctx: Context, reaction: Reaction) {
+		if let Err(why) = reaction.channel_id.say(
+			&ctx.http,
+			format!(
+				"{} left a {} reaction",
+				reaction.user(&ctx).unwrap().name,
+				match reaction.emoji {
+					ReactionType::Custom {
+						animated: _animated,
+						id: _id,
+						name,
+					} => name.unwrap(),
+					ReactionType::Unicode(uni) => uni,
+					ReactionType::__Nonexhaustive => String::new(),
+				}
+			),
+		) {
+			println!("Error reacting to a reaction: {:?}", why);
+		}
+	}
+
+	// prints a message to the console when ready
+	fn ready(&self, _: Context, ready: Ready) {
+		println!("{} is ready", ready.user.name);
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,54 @@
-extern crate serenity;
+mod events;
 
+use events::Handler;
+
+use serenity::client::Client;
 use serenity::model::channel::Message;
-use serenity::model::channel::Reaction;
-use serenity::model::channel::ReactionType;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
+use serenity::prelude::{Context};
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }, Args
+};
 
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Handler;
-
-impl EventHandler for Handler {
-    fn reaction_add(&self, ctx: Context, reaction: Reaction) {
-        if let Err(why) = reaction.channel_id.say(
-            &ctx.http,
-            format!(
-                "{} left a {} reaction",
-                reaction.user(&ctx).unwrap().name,
-                match reaction.emoji {
-                    ReactionType::Custom {
-                        animated: _animated,
-                        id: _id,
-                        name,
-                    } => name.unwrap(),
-                    ReactionType::Unicode(uni) => uni,
-                    ReactionType::__Nonexhaustive => String::new(),
-                }
-            ),
-        ) {
-            println!("Error reacting to a reaction: {:?}", why);
-        }
-    }
-
-    fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "?ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?") {
-                println!("Error giving message: {:?}", why);
-            }
-        }
-    }
-
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is ready", ready.user.name);
-    }
-}
+#[group]
+#[commands(ping, say)]
+struct General;
 
 fn main() {
-    let mut file = File::open("token").unwrap();
-    let mut token = String::new();
-    file.read_to_string(&mut token)
-        .expect("Token file not found");
+	let mut file = File::open("token").unwrap();
+	let mut token = String::new();
+	file.read_to_string(&mut token)
+		.expect("Token file not found");
 
-    let mut client = Client::new(&token, Handler).expect("Error creating client");
+	let mut client =
+		Client::new(&token, Handler).expect("Error creating client");
 
-    if let Err(msg) = client.start() {
-        println!("Error: {:?}", msg);
-    }
+	client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("?"))
+        .group(&GENERAL_GROUP));
+
+	if let Err(msg) = client.start() {
+		println!("Error: {:?}", msg);
+	}
+}
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+	msg.reply(&ctx, "Pong?")?;
+
+	Ok(())
+}
+
+#[command]
+fn say(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
+	msg.channel_id.say(&ctx.http, args.rest())?;
+
+	Ok(())
 }
